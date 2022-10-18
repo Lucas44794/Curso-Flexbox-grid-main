0c2b9b910590d1557c203b0d993ec24a021d152a
Lucas Rogerio da Silva <109930651+Lucas44794@users.noreply.github.com>
2022-10-18 15:53:18 -0300

    Ajustando para ficar igual do instrutor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const botaoMenu = document.querySelector('.cabecalho__menu')
-const menu = document.querySelector('.menu__lateral')
+const menu = document.querySelector('.menu-lateral')
 
 botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu__lateral--ativo')
+    menu.classList.toggle('menu-lateral--ativo')
 })
